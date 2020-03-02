8c8e63b3d767009a64b7ff638fc149a50fa7e5d3
ahmedqureshi131 <axq02670@ucmo.edu>
2020-03-02 12:18:58 -0600

    Updated Files

---
+++ admin/adminLogin.php
@@ -0,0 +1,33 @@
+<!-- this is the admin login -->
+
+<html>
+<head> 
+
+	<link href="Login.css" rel="stylesheet" type="text/css"
+	<link href="https://fonts.googleapis.com/css?family=Play" rel="stylesheet">
+	<title> Login Form </title>
+	
+
+
+<body>
+	<div class="logincontainer">
+		<h1> Schedule Planner </h1>
+		<form>
+			<p> Username </p>
+			<input type="text" name="" placeholder="Enter Username">
+			<p> Password </p>
+			<input type="password" name="" placeholder="Enter Password">
+			<input type="submit" name="" value="Login">
+			<a href="#"> Forgot password? </a><br>
+			<a href="registration.php"> Don't have an account? Sign up </a><br>
+			<a href="Login.php"> Are you a Student?</a>
+			
+		</form>
+	</div>
+ 
+
+</body>
+</head>
+</html>
+		
+		
---
+++ dbcon.php
@@ -0,0 +1,10 @@
+<?php
+$con = mysqli_connect('www.math-cs.ucmo.edu','S20studentsched','S20dehcstneduts','S20studentsched');
+
+if($con ==false)
+{
+	echo "connection is not done!!";
+}
+
+
+?>
